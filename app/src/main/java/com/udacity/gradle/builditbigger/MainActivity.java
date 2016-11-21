4c55e7b9f1b064aed47d2d9ace464f08607666ee
mailtoabhi15 <mailtoabhi15@users.noreply.github.com>
2016-11-21 19:08:35 +0530

    impl: Done with adding & deploying GCE on AppEngine

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,15 +1,25 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.abhishek.dixit.jokeapp.jokebackend.myApi.MyApi;
 import com.dixit.abhishek.android.jokeandroidlib.JokeActivity;
 import com.example.JokeWiz;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -45,15 +55,56 @@ public class MainActivity extends ActionBarActivity {
 
     public void tellJoke(View view) {
 
-        JokeWiz jokewiz =new JokeWiz();
-        String joke = jokewiz.tellJoke();
+//        JokeWiz jokewiz =new JokeWiz();
+//        String joke = jokewiz.tellJoke();
+//
+//        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
 
-        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Joking!!"));
 
-        Intent intent = new Intent(this, JokeActivity.class);
-        intent.putExtra("joke",joke);
-        startActivity(intent);
+
+    }
+}
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("https://jokebackend-deploy.appspot.com/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
     }
 
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
 
+        Intent intent = new Intent(context, JokeActivity.class);
+        intent.putExtra("joke",result);
+        context.startActivity(intent);
+    }
 }
--- jokebackend/src/main/java/com/abhishek/dixit/jokeapp/jokebackend/MyEndpoint.java
+++ jokebackend/src/main/java/com/abhishek/dixit/jokeapp/jokebackend/MyEndpoint.java
@@ -6,6 +6,7 @@
 
 package com.abhishek.dixit.jokeapp.jokebackend;
 
+import com.example.JokeWiz;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -26,13 +27,15 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
+    JokeWiz jokewiz =new JokeWiz();
+    String joke = jokewiz.tellJoke();
     /**
      * A simple endpoint method that takes a name and says Hi back
      */
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData("Hi, " + joke);
 
         return response;
     }
